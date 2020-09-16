f866555d2a7d9f1ae07b7b60c778ae580689084b
Prince <chimeremezechukwudire@gmail.com>
2020-09-16 12:02:15 +0100

    messaging area added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -23,3 +23,23 @@ const openUpgradeForm = () => {
     document.getElementById('upgrade_form').style.display = "block";
     document.getElementById('account').style.display = "none";
 }
+
+
+// toggle dropdown menu
+
+const showDropdown = () => {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
